b65237f7138f638ebda57710d96bc6093da3e287
cchoi-oliver <cchoi.oliver@gmail.com>
2019-06-30 23:07:18 -0400

    Implemented erasing and drawing logic

--- FontCodeGenerator.py
+++ FontCodeGenerator.py
@@ -52,6 +52,7 @@ class FontCodeGenerator:
         self.drawWindow.resize(550, 400)
         self.hLayout = QtWidgets.QHBoxLayout()
         self.vLayout = QtWidgets.QVBoxLayout()
+        self.drawLayout = QtWidgets.QVBoxLayout()
         self.pixelGrid = QtWidgets.QTableWidget()
         self.pixelGrid.horizontalHeader().setDefaultSectionSize(30)
         self.pixelGrid.verticalHeader().setDefaultSectionSize(30)
@@ -59,22 +60,29 @@ class FontCodeGenerator:
         self.pixelGrid.verticalHeader().hide()
         self.charList = QtWidgets.QListWidget()
         self.saveButton = QtWidgets.QPushButton(text="Save")
+        self.drawButton = QtWidgets.QPushButton(text="Draw")
+        self.eraseButton = QtWidgets.QPushButton(text="Erase")
+        self.drawBtnLayout = QtWidgets.QHBoxLayout()
 
         # Set Widget Actions Behavior
         self.saveButton.clicked.connect(self.startSave)
+        self.drawButton.clicked.connect(self.selectDraw)
+        self.eraseButton.clicked.connect(self.selectErase)
         self.pixelGrid.itemSelectionChanged.connect(self.onCellSelection)
         self.pixelPalette = self.pixelGrid.palette()
-        #self.pixelPalette.setBrush(QtGui.QPalette.Highlight, Qt.black)
-        self.pixelPalette.setBrush(QtGui.QPalette.Highlight, QtGui.QBrush(QtGui.QColor(0, 0, 0)))
-        self.pixelPalette.setBrush(QtGui.QPalette.HighlightedText, QtGui.QBrush(QtGui.QColor(0, 0, 0)))
-        self.pixelGrid.setPalette(self.pixelPalette)
+        self.selectDraw()
         self.pixelGridArray = np.zeros(1)
+
 
         # Build Drawing Window UI
         self.vLayout.addWidget(self.charList)
         self.vLayout.addWidget(self.saveButton)
         self.hLayout.addLayout(self.vLayout)
-        self.hLayout.addWidget(self.pixelGrid)
+        self.drawLayout.addWidget(self.pixelGrid)
+        self.drawBtnLayout.addWidget(self.drawButton)
+        self.drawBtnLayout.addWidget(self.eraseButton)
+        self.drawLayout.addLayout(self.drawBtnLayout)
+        self.hLayout.addLayout(self.drawLayout)
         self.hLayout.setStretch(0, 1)
         self.hLayout.setStretch(1, 2)
         self.drawWindow.setLayout(self.hLayout)
@@ -82,30 +90,54 @@ class FontCodeGenerator:
         self.mainWindow.show()
 
     def startNew(self):
-        print("Hello")
+        print("Making new font")
         self.mainWindow.close()
         rows = int(self.heightTextInput.text())
         cols = int(self.widthTextInput.text())
         self.pixelGrid.setRowCount(rows)
         self.pixelGrid.setColumnCount(cols)
         self.pixelGridArray = np.zeros((rows, cols))
+        # Allows drawing on the
+        for x in range(rows):
+            for y in range(cols):
+                self.pixelGrid.setItem(x, y, QtWidgets.QTableWidgetItem())
         print(self.pixelGridArray)
         self.drawWindow.show()
 
     def startLoad(self):
-        print("Hiya")
+        print("Loading font")
         self.mainWindow.close()
         self.drawWindow.show()
 
     def startSave(self):
         print(self.pixelGridArray)
 
+    def selectDraw(self):
+        self.drawButton.setStyleSheet("background-color: #70a5fa")
+        self.eraseButton.setStyleSheet("background-color: grey")
+        self.pixelGrid.clearSelection()
+        self.pixelPalette.setBrush(QtGui.QPalette.Highlight, QtGui.QBrush(QtGui.QColor(0, 0, 0)))
+        self.pixelPalette.setBrush(QtGui.QPalette.HighlightedText, QtGui.QBrush(QtGui.QColor(0, 0, 0)))
+        self.pixelGrid.setPalette(self.pixelPalette)
+        self.drawMode = True
+
+    def selectErase(self):
+        self.drawButton.setStyleSheet("background-color: grey")
+        self.eraseButton.setStyleSheet("background-color: #70a5fa")
+        self.pixelGrid.clearSelection()
+        self.pixelPalette.setBrush(QtGui.QPalette.Highlight, QtGui.QBrush(QtGui.QColor(255, 255, 255)))
+        self.pixelPalette.setBrush(QtGui.QPalette.HighlightedText, QtGui.QBrush(QtGui.QColor(255, 255, 255)))
+        self.pixelGrid.setPalette(self.pixelPalette)
+        self.drawMode = False
+
     def onCellSelection(self):
         for x in self.pixelGrid.selectedIndexes():
-            self.pixelGridArray[x.row(), x.column()] = 1
-            self.pixelGrid.setItem(x.row(), x.column(), QtWidgets.QTableWidgetItem())
-            self.pixelGrid.item(x.row(), x.column()).setBackground(QtGui.QColor(0, 0, 0))
-        #print(self.pixelGridArray)
+            if self.drawMode:
+                self.pixelGridArray[x.row(), x.column()] = 1
+                self.pixelGrid.item(x.row(), x.column()).setBackground(QtGui.QColor(0, 0, 0))
+            else:
+                self.pixelGridArray[x.row(), x.column()] = 0
+                self.pixelGrid.item(x.row(), x.column()).setBackground(QtGui.QColor(255, 255, 255))
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
