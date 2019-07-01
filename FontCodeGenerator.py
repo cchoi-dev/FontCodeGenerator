5f266ea6bcdbc04e0c3bef3ff65f99601b52569e
cchoi-oliver <cchoi.oliver@gmail.com>
2019-06-30 22:25:00 -0400

    Create FontCodeGenerator.py

---
+++ FontCodeGenerator.py
@@ -0,0 +1,103 @@
+import sys
+import os
+from PyQt5 import QtGui, QtCore, QtWidgets, Qt
+
+
+class pixelGrid(QtWidgets.QTableView):
+    def __init__(self):
+        super().__init__()
+        self.activated.connect(self.onClick)
+
+    def onClick(self):
+        pass
+        # currCell = self.
+
+
+class FontCodeGenerator:
+
+    def __init__(self):
+        super(FontCodeGenerator, self).__init__()
+        # --- Main Menu Window  --- #
+        self.mainWindow = QtWidgets.QWidget()
+        self.titleLabel = QtWidgets.QLabel("Font Code Generator for C")
+        self.mainWinLayout = QtWidgets.QVBoxLayout()
+        self.newButton = QtWidgets.QPushButton(text="New")
+        self.loadButton = QtWidgets.QPushButton(text="Load")
+        self.widthTextLayout = QtWidgets.QVBoxLayout()
+        self.heightTextLayout = QtWidgets.QVBoxLayout()
+        self.widthTextInput = QtWidgets.QLineEdit(text="7")
+        self.heightTextInput = QtWidgets.QLineEdit(text="10")
+        self.textInputLayout = QtWidgets.QHBoxLayout()
+
+        # Set Widget Actions/Behavior
+        self.newButton.clicked.connect(self.startNew)
+        self.loadButton.clicked.connect(self.startLoad)
+
+        # Build Main Window UI
+        self.widthTextLayout.addWidget(QtWidgets.QLabel("Width:"))
+        self.heightTextLayout.addWidget(QtWidgets.QLabel("Height:"))
+        self.widthTextLayout.addWidget(self.widthTextInput)
+        self.heightTextLayout.addWidget(self.heightTextInput)
+        self.textInputLayout.addLayout(self.widthTextLayout)
+        self.textInputLayout.addLayout(self.heightTextLayout)
+        self.mainWinLayout.addWidget(self.titleLabel)
+        self.mainWinLayout.addLayout(self.textInputLayout)
+        self.mainWinLayout.addWidget(self.newButton)
+        self.mainWinLayout.addWidget(self.loadButton)
+        self.mainWindow.setLayout(self.mainWinLayout)
+
+        # --- Drawing UI --- #
+        self.drawWindow = QtWidgets.QWidget()
+        self.drawWindow.resize(550, 400)
+        self.hLayout = QtWidgets.QHBoxLayout()
+        self.vLayout = QtWidgets.QVBoxLayout()
+        self.pixelGrid = QtWidgets.QTableWidget()
+        self.pixelGrid.horizontalHeader().setDefaultSectionSize(30)
+        self.pixelGrid.verticalHeader().setDefaultSectionSize(30)
+        self.pixelGrid.horizontalHeader().hide()
+        self.pixelGrid.verticalHeader().hide()
+        self.charList = QtWidgets.QListWidget()
+        self.saveButton = QtWidgets.QPushButton(text="Save")
+
+        # Set Widget Actions Behavior
+        self.saveButton.clicked.connect(self.startSave)
+        self.pixelGrid.itemSelectionChanged.connect(self.onCellSelection)
+        self.pixelPalette = self.pixelGrid.palette()
+        self.pixelPalette.setBrush(QtGui.QPalette.Highlight, Qt.black)
+        #self.pixelPalette.setBrush(QtGui.QPalette.Highlight, QtGui.QBrush(QtGui.QColor(0, 0, 0)))
+        self.pixelPalette.setBrush(QtGui.QPalette.HighlightedText, QtGui.QBrush(QtGui.QColor(0, 0, 0)))
+
+        # Build Drawing Window UI
+        self.vLayout.addWidget(self.charList)
+        self.vLayout.addWidget(self.saveButton)
+        self.hLayout.addLayout(self.vLayout)
+        self.hLayout.addWidget(self.pixelGrid)
+        self.hLayout.setStretch(0, 1)
+        self.hLayout.setStretch(1, 2)
+        self.drawWindow.setLayout(self.hLayout)
+
+        self.mainWindow.show()
+
+    def startNew(self):
+        print("Hello")
+        self.mainWindow.close()
+        self.pixelGrid.setRowCount(int(self.heightTextInput.text()))
+        self.pixelGrid.setColumnCount(int(self.widthTextInput.text()))
+        self.drawWindow.show()
+
+    def startLoad(self):
+        print("Hiya")
+        self.mainWindow.close()
+        self.drawWindow.show()
+
+    def startSave(self):
+        pass
+
+    def onCellSelection(self):
+        print("SD")
+        pass
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    program = FontCodeGenerator()
+    sys.exit(app.exec_())
